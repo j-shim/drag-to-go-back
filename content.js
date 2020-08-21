d9fe35d83a119eea53c1bbb965931635bcfec952
June Shim <jys2@sfu.ca>
2020-08-21 02:23:22 -0700

    Implement quick left-click dragging

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 let dragX1 = null
 let dragX2 = null
+let time1InMilliseconds = null
+let time2InMilliseconds = null
 // const DRAG_THRESHOLD = 100
 chrome.storage.local.get('DRAG_THRESHOLD', data => {
   if (data.DRAG_THRESHOLD === undefined) {
@@ -11,38 +13,57 @@ chrome.storage.local.get('DRAG_THRESHOLD', data => {
   }
 })
 
+chrome.storage.local.get('DRAG_TIME_LIMIT_IN_MILLISECONDS', data => {
+  if (data.DRAG_TIME_LIMIT_IN_MILLISECONDS === undefined) {
+    chrome.storage.local.set({ DRAG_TIME_LIMIT_IN_MILLISECONDS: 400 }, _ => {
+      console.log('DRAG-TO-GO-BACK Time Limit: 400 Milliseconds')
+    })
+  } else {
+    console.log(`DRAG-TO-GO-BACK Time Limit: ${data.DRAG_TIME_LIMIT_IN_MILLISECONDS} Milliseconds`)
+  }
+})
+
 window.addEventListener('mousedown', event => {
   // const metaKey = navigator.platform.includes('Mac') ? event.metaKey : event.ctrlKey
   // if (event.button === 0 && metaKey) {
   //   dragX1 = event.clientX
   // }
-  if (event.button === 1) {
+  if (event.button === 0) {
     dragX1 = event.clientX
+    time1InMilliseconds = Date.now()
   }
 })
 
 window.addEventListener('mouseup', event => {
-  if (event.button === 1 && dragX1 !== null) {
+  if (event.button === 0 && dragX1 !== null && time1InMilliseconds !== null) {
     dragX2 = event.clientX
+    time2InMilliseconds = Date.now()
 
     const deltaX = Math.abs(dragX2 - dragX1)
+    const deltaTime = time2InMilliseconds - time1InMilliseconds
 
-
-    chrome.storage.local.get('DRAG_THRESHOLD', data => {
+    chrome.storage.local.get(['DRAG_THRESHOLD', 'DRAG_TIME_LIMIT_IN_MILLISECONDS'], data => {
       const DRAG_THRESHOLD = data.DRAG_THRESHOLD
+      const DRAG_TIME_LIMIT_IN_MILLISECONDS = data.DRAG_TIME_LIMIT_IN_MILLISECONDS
 
       console.log(`DRAG-TO-GO-BACK Threshold: ${DRAG_THRESHOLD} Pixels`)
-      if (deltaX > DRAG_THRESHOLD) {
+      console.log(`DRAG-TO-GO-BACK Time Limit: ${DRAG_TIME_LIMIT_IN_MILLISECONDS} Milliseconds`)
+
+      if (deltaX > DRAG_THRESHOLD && deltaTime < DRAG_TIME_LIMIT_IN_MILLISECONDS) {
 
         if (dragX1 < dragX2) {
           // Go back
           dragX1 = null
           dragX2 = null
+          time1InMilliseconds = null
+          time2InMilliseconds = null
           history.back()
         } else {
           // Go forward
           dragX1 = null
           dragX2 = null
+          time1InMilliseconds = null
+          time2InMilliseconds = null
           history.forward()
         }
 
@@ -50,6 +71,8 @@ window.addEventListener('mouseup', event => {
         // Do nothing
         dragX1 = null
         dragX2 = null
+        time1InMilliseconds = null
+        time2InMilliseconds = null
       }
     })
   }
--- options.js
+++ options.js
@@ -1,12 +1,23 @@
-const update = (event) => {
+const updateThreshold = (event) => {
   const value = Number(event.target.value)
   chrome.storage.local.set({ DRAG_THRESHOLD: value }, _ => {
-    alert(`Drag Threshold changed to ${value} pixels`)
+    alert(`DRAG-TO-GO-BACK Threshold changed to ${value} Pixels`)
+  })
+}
+const updateTimeLimit = (event) => {
+  const value = Number(event.target.value)
+  chrome.storage.local.set({ DRAG_TIME_LIMIT_IN_MILLISECONDS: value }, _ => {
+    alert(`DRAG-TO-GO-BACK Time Limit changed to ${value} Milliseconds`)
   })
 }
 
-const inputs = document.getElementsByClassName('drag-threshold')
+const dragThresholdInputs = document.getElementsByClassName('drag-threshold')
+const dragTimeLimitInputs = document.getElementsByClassName('drag-time-limit')
 
-for (let input of inputs) {
-  input.addEventListener('input', update)
+for (let input of dragThresholdInputs) {
+  input.addEventListener('input', updateThreshold)
+}
+
+for (let input of dragTimeLimitInputs) {
+  input.addEventListener('input', updateTimeLimit)
 }
