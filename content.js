563015c7ebbd77483be0cccb6e93883447f462be
June Shim <jys2@sfu.ca>
2020-09-26 23:48:46 -0700

    Add console.log ON/OFF switch

--- content.js
+++ content.js
@@ -3,23 +3,65 @@ let dragX2 = null
 let time1InMilliseconds = null
 let time2InMilliseconds = null
 // const DRAG_THRESHOLD = 100
-chrome.storage.local.get('DRAG_THRESHOLD', data => {
+// chrome.storage.local.get('DRAG_THRESHOLD', data => {
+//   if (data.DRAG_THRESHOLD === undefined) {
+//     chrome.storage.local.set({ DRAG_THRESHOLD: 100 }, _ => {
+//       console.log('DRAG-TO-GO-BACK Threshold: 100 Pixels')
+//     })
+//   } else {
+//     console.log(`DRAG-TO-GO-BACK Threshold: ${data.DRAG_THRESHOLD} Pixels`)
+//   }
+// })
+
+// chrome.storage.local.get('DRAG_TIME_LIMIT_IN_MILLISECONDS', data => {
+//   if (data.DRAG_TIME_LIMIT_IN_MILLISECONDS === undefined) {
+//     chrome.storage.local.set({ DRAG_TIME_LIMIT_IN_MILLISECONDS: 400 }, _ => {
+//       console.log('DRAG-TO-GO-BACK Time Limit: 400 Milliseconds')
+//     })
+//   } else {
+//     console.log(`DRAG-TO-GO-BACK Time Limit: ${data.DRAG_TIME_LIMIT_IN_MILLISECONDS} Milliseconds`)
+//   }
+// })
+
+// chrome.storage.local.get('DRAG_LOG_SWITCH', data => {
+//   if (data.DRAG_LOG_SWITCH === undefined) {
+//     chrome.storage.local.set({ DRAG_LOG_SWITCH: 'ON' }, _ => {
+//       console.log('DRAG-TO-GO-BACK Log Switch: ON')
+//     })
+//   } else {
+//     console.log(`DRAG-TO-GO-BACK Log Switch: ${data.DRAG_LOG_SWITCH}`)
+//   }
+// })
+
+const logger = (logSwitch, message) => {
+  if (logSwitch === undefined || logSwitch === 'ON') {
+    console.log(message)
+  }
+}
+
+chrome.storage.local.get(['DRAG_THRESHOLD', 'DRAG_TIME_LIMIT_IN_MILLISECONDS', 'DRAG_LOG_SWITCH', ], data => {
+  if (data.DRAG_LOG_SWITCH === undefined) {
+    chrome.storage.local.set({ DRAG_LOG_SWITCH: 'ON' }, _ => {
+      logger(data.DRAG_LOG_SWITCH, 'DRAG-TO-GO-BACK Log Switch: ON')
+    })
+  } else {
+    logger(data.DRAG_LOG_SWITCH, `DRAG-TO-GO-BACK Log Switch: ${data.DRAG_LOG_SWITCH}`)
+  }
+
   if (data.DRAG_THRESHOLD === undefined) {
     chrome.storage.local.set({ DRAG_THRESHOLD: 100 }, _ => {
-      console.log('DRAG-TO-GO-BACK Threshold: 100 Pixels')
+      logger(data.DRAG_LOG_SWITCH, 'DRAG-TO-GO-BACK Threshold: 100 Pixels')
     })
   } else {
-    console.log(`DRAG-TO-GO-BACK Threshold: ${data.DRAG_THRESHOLD} Pixels`)
+    logger(data.DRAG_LOG_SWITCH, `DRAG-TO-GO-BACK Threshold: ${data.DRAG_THRESHOLD} Pixels`)
   }
-})
 
-chrome.storage.local.get('DRAG_TIME_LIMIT_IN_MILLISECONDS', data => {
   if (data.DRAG_TIME_LIMIT_IN_MILLISECONDS === undefined) {
     chrome.storage.local.set({ DRAG_TIME_LIMIT_IN_MILLISECONDS: 400 }, _ => {
-      console.log('DRAG-TO-GO-BACK Time Limit: 400 Milliseconds')
+      logger(data.DRAG_LOG_SWITCH, 'DRAG-TO-GO-BACK Time Limit: 400 Milliseconds')
     })
   } else {
-    console.log(`DRAG-TO-GO-BACK Time Limit: ${data.DRAG_TIME_LIMIT_IN_MILLISECONDS} Milliseconds`)
+    logger(data.DRAG_LOG_SWITCH, `DRAG-TO-GO-BACK Time Limit: ${data.DRAG_TIME_LIMIT_IN_MILLISECONDS} Milliseconds`)
   }
 })
 
@@ -42,12 +84,13 @@ window.addEventListener('mouseup', event => {
     const deltaX = Math.abs(dragX2 - dragX1)
     const deltaTime = time2InMilliseconds - time1InMilliseconds
 
-    chrome.storage.local.get(['DRAG_THRESHOLD', 'DRAG_TIME_LIMIT_IN_MILLISECONDS'], data => {
+    chrome.storage.local.get(['DRAG_THRESHOLD', 'DRAG_TIME_LIMIT_IN_MILLISECONDS', 'DRAG_LOG_SWITCH', ], data => {
       const DRAG_THRESHOLD = data.DRAG_THRESHOLD
       const DRAG_TIME_LIMIT_IN_MILLISECONDS = data.DRAG_TIME_LIMIT_IN_MILLISECONDS
+      const DRAG_LOG_SWITCH = data.DRAG_LOG_SWITCH
 
-      console.log(`DRAG-TO-GO-BACK Threshold: ${DRAG_THRESHOLD} Pixels`)
-      console.log(`DRAG-TO-GO-BACK Time Limit: ${DRAG_TIME_LIMIT_IN_MILLISECONDS} Milliseconds`)
+      logger(DRAG_LOG_SWITCH, `DRAG-TO-GO-BACK Threshold: ${DRAG_THRESHOLD} Pixels`)
+      logger(DRAG_LOG_SWITCH, `DRAG-TO-GO-BACK Time Limit: ${DRAG_TIME_LIMIT_IN_MILLISECONDS} Milliseconds`)
 
       if (deltaX > DRAG_THRESHOLD && deltaTime < DRAG_TIME_LIMIT_IN_MILLISECONDS) {
 
--- options.js
+++ options.js
@@ -10,9 +10,16 @@ const updateTimeLimit = (event) => {
     alert(`DRAG-TO-GO-BACK Time Limit changed to ${value} Milliseconds`)
   })
 }
+const updateLogSwitch = (event) => {
+  const value = event.target.value
+  chrome.storage.local.set({ DRAG_LOG_SWITCH: value }, _ => {
+    alert(`DRAG-TO-GO-BACK Log Switch changed to ${value}`)
+  })
+}
 
 const dragThresholdInputs = document.getElementsByClassName('drag-threshold')
 const dragTimeLimitInputs = document.getElementsByClassName('drag-time-limit')
+const dragLogSwitchInputs = document.getElementsByClassName('drag-log-switch')
 
 for (let input of dragThresholdInputs) {
   input.addEventListener('input', updateThreshold)
@@ -20,4 +27,8 @@ for (let input of dragThresholdInputs) {
 
 for (let input of dragTimeLimitInputs) {
   input.addEventListener('input', updateTimeLimit)
+}
+
+for (let input of dragLogSwitchInputs) {
+  input.addEventListener('input', updateLogSwitch)
 }
